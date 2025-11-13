3e3f43de76332b063d24ecd9d9e1203c2b611c00
nnndaru <aji.prabandaru@gmail.com>
2025-11-13 16:07:51 +0700

    Update

--- index.js
+++ index.js
@@ -156,7 +156,7 @@ class UIHandler {
   // Menampilkan animasi loading di console
   static showLoadingAnimation(text) {
     return new Promise(resolve => {
-      const BAR_LENGTH = 50;
+      const BAR_LENGTH = 40;
       let elapsed = 0;
       const intervalTime = 10;
       const duration = CONFIG.LOADING_DURATION;
